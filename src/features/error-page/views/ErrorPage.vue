5f942540d6b90ebb152c3abe768d454f80ba54dd
Alain Boudard <coco.boudard@gmail.com>
2026-04-03 22:28:50 +0200

    feat(error-page): add ErrorPage component and route; integrate Sentry for error tracking

---
+++ src/features/error-page/views/ErrorPage.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { onMounted } from 'vue';
+
+onMounted(() => {
+  ; (globalThis as { myUndefinedFunction?: () => void }).myUndefinedFunction!()
+})
+</script>
+
+<template>
+  <div class="p-3">
+    <h2>Error page</h2>
+  </div>
+</template>
